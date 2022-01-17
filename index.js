9b495588da082630872a7d3e011d7272996deba6
Haroon <muhammadharoon1592@gmail.com>
2022-01-18 00:34:58 +0500

    Added pagination feature

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ let sb2=document.getElementById("sb2");
 let wb2=document.getElementById("wb2");
 let cb2=document.getElementById("cb2");
 
+
+
 cmb.onclick=function(){
     cd.style.display="block";
     if(cd.style.display=="block"){
@@ -34,20 +36,22 @@ cmb.onclick=function(){
 
 hb.onclick=function(){
 hd.style.display="block";
+hb.classList.add("activate");
 if(hd.style.display=="block"){
     ad.style.display="none";
     sd.style.display="none";
     wd.style.display="none";
-    cd.style.display="none"; 
+    cd.style.display="none";
     }
 }
 ab.onclick=function(){
 ad.style.display="block";
+ab.classList.add("activate");
 if(ad.style.display=="block"){
     hd.style.display="none";
     sd.style.display="none";
     wd.style.display="none";
-    cd.style.display="none"; 
+    cd.style.display="none";
     }  
 } 
 sb.onclick=function(){
@@ -128,6 +132,33 @@ wb2.onclick=function(){
              sd.style.display="none"; 
         }
     }
+
+let pag1=document.getElementById("pag1");
+let pag2=document.getElementById("pag2");
+let wp1=document.getElementById("wp1");
+let wp2=document.getElementById("wp2");
+
+pag1.onclick=function(){
+    wp1.style.display="block";
+        if(wp1.style.display=="block"){
+             wp2.style.display="none";
+             pag2.classList.remove("active");
+             pag1.classList.add("active");
+             
+        }
+    }
+
+pag2.onclick=function(){
+    wp2.style.display="block";
+        if(wp2.style.display=="block"){
+             wp1.style.display="none";
+             pag1.classList.remove("active");
+             pag2.classList.add("active");
+        }
+    }
+
+
+
     
 
 document.addEventListener('DOMContentLoaded', function() {
